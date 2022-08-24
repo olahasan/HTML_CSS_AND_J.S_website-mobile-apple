abaea65859ec79ed79c014678022147fb93f70c5
olahasan <ola.ali1000@gmail.com>
2022-08-24 10:53:11 -0700

    Upload The Design

---
+++ main.js
@@ -0,0 +1,30 @@
+let main = document.getElementById("main");
+let p1 = document.getElementById("p1");
+let p2 = document.getElementById("p2");
+let p3 = document.getElementById("p3");
+let p4 = document.getElementById("p4");
+let p5 = document.getElementById("p5");
+
+p1.onclick = function () {
+    main.src = p1.src
+}
+p2.onclick = function () {
+    main.src = p2.src
+}
+p3.onclick = function () {
+    main.src = p3.src
+}
+p4.onclick = function () {
+    main.src = p4.src
+}
+p5.onclick = function () {
+    main.src = p5.src
+}
+
+console.log(main)
+console.log(p1)
+console.log(p2)
+console.log(p3)
+console.log(p4)
+console.log(p5)
+
